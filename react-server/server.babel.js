e63ef145cd35e1522041301f3f4bb05a962ab150
finalight <finalight@gmail.com>
2016-09-05 17:40:14 +0800

    refactor

--- react-server/server.babel.js
+++ react-server/server.babel.js
@@ -2,6 +2,7 @@ import express from 'express';
 const app = require('express')();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const async = require('async');
 
 let gameBoard = [
     [0, 0, 0],
@@ -15,30 +16,57 @@ let player2ID = null;
 io.on('connection', function(socket) {
     playerCount++;
 
-    if (player1ID == null) {
+    if (playerCount == 1) {
         player1ID = socket.id;
-    } else if (player2ID == null) {
+        console.log("assign to player 1")
+    } else if (playerCount == 2) {
         player2ID = socket.id;
+        console.log("assign to player 2")
     }
 
     console.log(playerCount)
-    socket.join("room1")
+
+    if (playerCount <= 2) {
+        socket.join("room1")
+    }
+
+    if (playerCount == 1) {
+        socket.to(player1ID).emit('assignTurn', {
+            player1: player1,
+            playerID: player1ID,
+            player2ID: player2ID
+        });
+    } else if (playerCount == 2) {
+        socket.to(player2ID).emit('assignTurn', {
+            player1: !player1,
+            playerID: player2ID,
+            player2ID: player2ID
+        });
+    }
 
     socket.to("room1").emit('gameOn', {
         game: gameBoard,
     });
 
-    socket.to(player1ID).emit('assignTurn', {
-        player1: player1
-    });
-
-    socket.to(player2ID).emit('assignTurn', {
-        player1: !player1
-    });
 
     socket.on('disconnect', function() {
         playerCount--;
-        console.log(playerCount)
+
+        if (playerCount <= 0) {
+            gameBoard = [
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0]
+            ]
+        }
+
+        if (playerCount == 1) {
+            player2ID = null;
+            console.log("removing player 2")
+        } else if (playerCount == 0) {
+            player1ID = null;
+            console.log("removing player 1")
+        }
     });
     socket.on('whatever', function(data) {
         gameBoard = data;
@@ -50,9 +78,22 @@ io.on('connection', function(socket) {
             player1: player1
         });
     });
+
+    app.get("/", function(req, res) {
+        socket.broadcast.to(player2ID).emit('assignTurn', {
+            player1: !player1,
+            playerID: player2ID,
+            player2ID: player2ID
+        });
+        socket.broadcast.to(player1ID).emit('assignTurn', {
+            player1: player1,
+            playerID: player1ID,
+            player2ID: player2ID
+        });
+        res.send("hah")
+    });
 });
 
 
-app.use('/', express.static('public'));
-
+app.use('/haha', express.static('public'));
 server.listen(5000);
