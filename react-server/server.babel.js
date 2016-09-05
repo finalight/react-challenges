20b5440f8c1fea5f5f86f12616f7a602c2182901
finalight <finalight@gmail.com>
2016-09-05 16:37:07 +0800

    change code

--- react-server/server.babel.js
+++ react-server/server.babel.js
@@ -28,11 +28,11 @@ io.on('connection', function(socket) {
         game: gameBoard,
     });
 
-    socket.broadcast.to(player1ID).emit('assignTurn', {
+    socket.to(player1ID).emit('assignTurn', {
         player1: player1
     });
 
-    socket.broadcast.to(player2ID).emit('assignTurn', {
+    socket.to(player2ID).emit('assignTurn', {
         player1: !player1
     });
 
